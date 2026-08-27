8153d56811b2c4779b9a4320c5e0cefcd907d0dd
internobras <internobras@gmail.com>
2026-08-27 12:40:02 +0200

    Add FIBARO Direct configuration

---
+++ config.js
@@ -0,0 +1,10 @@
+window.FIBARO_CONFIG = {
+  whatsappNumber: '34633671657',
+  phoneDisplay: '633 671 657',
+  instagram: 'fibaroteleco',
+  tiktok: 'fiabroteleco',
+  domain: 'fibaroteleco.com',
+  supabaseUrl: '',
+  supabasePublishableKey: '',
+  leadCaptureEnabled: false
+};
